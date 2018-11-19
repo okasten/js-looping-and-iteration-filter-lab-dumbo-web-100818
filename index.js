30a8e9804691c7e92059db2fada94c3d3ca973de
Olivia Kasten <odkasten@gmail.com>
2018-11-19 02:40:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+function findMatching(array, str){
+  return array.filter(function (name) {return name.toLowerCase() === str.toLowerCase()})
+}
+
+function fuzzyMatch(array, str){
+  let l = str.length;
+  return array.filter(function (name) {return name.slice(0, l) === str})
+}
+
+function matchName(array, str){
+  return array.filter(function (driver) {return driver.name === str})
+}
